40db4b61e88cce3bf9ecb4979a2334fdfcf8f1e0
zhangyu1818 <723156735@qq.com>
2021-07-24 14:19:51 +0800

    tailwindcss 2.2

--- components/mask.tsx
+++ components/mask.tsx
@@ -31,8 +31,8 @@ const Mask = ({ position }: MaskProps) => (
   <div
     className={
       position === 'top'
-        ? 'fixed top-0 h-10 w-full z-10 transition-colors duration-700 mask-shadow'
-        : 'fixed bottom-0 h-10 w-full z-10 transform rotate-180 mask-shadow'
+        ? 'fixed top-0 h-10 w-full z-10 mask-shadow'
+        : 'fixed bottom-0 h-10 w-full z-10 rotate-180 mask-shadow'
     }
   />
 )
