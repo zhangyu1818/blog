367b3ca757b0686923f10be9531353310997b9d6
Yu <723156735@qq.com>
2022-12-27 10:59:54 +0800

    sitemap, prefetch

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [Astro](https://github.com/withastro/astro)
 
 [TailwindCSS](https://github.com/tailwindlabs/tailwindcss)
+
+[Next.js博客](https://github.com/zhangyu1818/blog/tree/nextjs)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-})
+  site: 'https://zhangyu.dev',
+  integrations: [tailwind(), sitemap(), prefetch()]
+});
