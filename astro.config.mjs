e0f8d16a0d2d94b703cfc59356056fb91164a365
Yu <723156735@qq.com>
2023-06-23 17:20:10 +0800

    update astro v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,6 @@ import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zhangyu.dev',
+  site: 'https://zhangyu.dev/',
   integrations: [tailwind(), sitemap(), prefetch(), robotsTxt()],
 })
